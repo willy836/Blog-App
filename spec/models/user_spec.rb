86887dba1b196f173f768516a7ad3df8fcbcf559
willy836 <wilsonkimiri836@gmail.com>
2023-03-30 13:15:29 +0300

    Add test for recent_posts method for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,23 +5,31 @@ RSpec.describe User, type: :model do
     
     before { subject.save }
 
-    it 'name should be present' do
-        subject.name = nil
-        expect(subject).to_not be_valid
+    describe 'Test User validations' do
+        it 'name should be present' do
+            subject.name = nil
+            expect(subject).to_not be_valid
+        end
+    
+        it 'posts_counter should be present' do
+            subject.posts_counter = nil
+            expect(subject).to_not be_valid
+        end
+    
+        it 'posts_counter should be an integer' do
+            subject.posts_counter = 'a'
+            expect(subject).to_not be_valid
+        end
+    
+        it 'posts_counter should be greater than or equal to zero' do
+            subject.posts_counter = -1
+            expect(subject).to_not be_valid
+        end
     end
 
-    it 'posts_counter should be present' do
-        subject.posts_counter = nil
-        expect(subject).to_not be_valid
+    describe '#recent_posts' do
+    it 'should return 3 most recent posts' do
+        expect(subject.recent_posts).to eq(subject.posts.last(3))
     end
-
-    it 'posts_counter should be an integer' do
-        subject.posts_counter = 'a'
-        expect(subject).to_not be_valid
-    end
-
-    it 'posts_counter should be greater than or equal to zero' do
-        subject.posts_counter = -1
-        expect(subject).to_not be_valid
-    end
+end
 end
