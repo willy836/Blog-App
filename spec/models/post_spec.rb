f2e562796c636c6602c84c1f6a3cdaea4f657257
willy836 <wilsonkimiri836@gmail.com>
2023-03-30 13:18:02 +0300

    Fix linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,62 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    first_user = User.create(name: 'Wilson', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
-    bio: 'Hi! I am a web developer', posts_counter: 0)
+  first_user = User.create(name: 'Wilson', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                           bio: 'Hi! I am a web developer', posts_counter: 0)
 
-    subject do
-      Post.new(author: first_user, title: 'Post One', text: 'This is my first post', comments_counter: 10, 
-      likes_counter: 50)
-    end   
+  subject do
+    Post.new(author: first_user, title: 'Post One', text: 'This is my first post', comments_counter: 10,
+             likes_counter: 50)
+  end
 
-    before { subject.save }
+  before { subject.save }
 
-    describe 'Test Post validations' do
-        it 'title should be present' do
-            subject.title = nil
-            expect(subject).to_not be_valid
-        end
-    
-        it 'title should not be too long' do
-            subject.title = 'a' * 251
-            expect(subject).to_not be_valid
-        end
-    
-        it 'comments_counter should be present' do
-            subject.comments_counter = nil
-            expect(subject).to_not be_valid
-        end
-    
-        it 'comments_counter should be an integer' do
-            subject.comments_counter = 'a'
-            expect(subject).to_not be_valid
-        end
-    
-        it 'comments_counter should be greater than or equal to zero' do
-            subject.comments_counter = -1
-            expect(subject).to_not be_valid
-        end
-    
-        it 'likes_counter should be present' do
-            subject.likes_counter = nil
-            expect(subject).to_not be_valid
-        end
-    
-        it 'likes_counter should be an integer' do
-            subject.likes_counter = 'a'
-            expect(subject).to_not be_valid
-        end
-    
-        it 'likes_counter should be greater than or equal to zero' do
-            subject.likes_counter = -1
-            expect(subject).to_not be_valid
-        end
+  describe 'Test Post validations' do
+    it 'title should be present' do
+      subject.title = nil
+      expect(subject).to_not be_valid
     end
 
-    describe '#show_recent_comments' do
-        it 'should return 5 most recent comments' do
-        expect(subject.show_recent_comments).to eq(subject.comments.last(5))
-        end
+    it 'title should not be too long' do
+      subject.title = 'a' * 251
+      expect(subject).to_not be_valid
     end
-    
+
+    it 'comments_counter should be present' do
+      subject.comments_counter = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'comments_counter should be an integer' do
+      subject.comments_counter = 'a'
+      expect(subject).to_not be_valid
+    end
+
+    it 'comments_counter should be greater than or equal to zero' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'likes_counter should be present' do
+      subject.likes_counter = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'likes_counter should be an integer' do
+      subject.likes_counter = 'a'
+      expect(subject).to_not be_valid
+    end
+
+    it 'likes_counter should be greater than or equal to zero' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#show_recent_comments' do
+    it 'should return 5 most recent comments' do
+      expect(subject.show_recent_comments).to eq(subject.comments.last(5))
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    subject { User.new(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer from USA.', posts_counter: 10) }
-    
-    before { subject.save }
+  subject { User.new(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer from USA.', posts_counter: 10) }
 
-    describe 'Test User validations' do
-        it 'name should be present' do
-            subject.name = nil
-            expect(subject).to_not be_valid
-        end
-    
-        it 'posts_counter should be present' do
-            subject.posts_counter = nil
-            expect(subject).to_not be_valid
-        end
-    
-        it 'posts_counter should be an integer' do
-            subject.posts_counter = 'a'
-            expect(subject).to_not be_valid
-        end
-    
-        it 'posts_counter should be greater than or equal to zero' do
-            subject.posts_counter = -1
-            expect(subject).to_not be_valid
-        end
+  before { subject.save }
+
+  describe 'Test User validations' do
+    it 'name should be present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
     end
 
-    describe '#recent_posts' do
+    it 'posts_counter should be present' do
+      subject.posts_counter = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts_counter should be an integer' do
+      subject.posts_counter = 'a'
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts_counter should be greater than or equal to zero' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#recent_posts' do
     it 'should return 3 most recent posts' do
-        expect(subject.recent_posts).to eq(subject.posts.last(3))
+      expect(subject.recent_posts).to eq(subject.posts.last(3))
     end
-end
+  end
 end
