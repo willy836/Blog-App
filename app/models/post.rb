509361bfe31a3abe860869c770db4459d9889bcf
willy836 <wilsonkimiri836@gmail.com>
2023-03-30 07:40:30 +0300

    Refactor to use after_save active callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
-
   def show_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_posts_counter
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
