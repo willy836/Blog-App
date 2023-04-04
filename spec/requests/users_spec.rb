af085174e1e35959cb4192522e8d518460860e15
willy836 <wilsonkimiri836@gmail.com>
2023-04-04 11:43:54 +0300

    Fix linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,36 +2,36 @@ require 'rails_helper'
 require 'rails-controller-testing'
 
 RSpec.describe UsersController, type: :request do
-  describe "GET #index" do
+  describe 'GET #index' do
     before { get users_path }
 
-    it "returns http successs" do
+    it 'returns http successs' do
       expect(response).to have_http_status(:success)
     end
 
-    it "renders index template" do
+    it 'renders index template' do
       expect(response).to render_template(:index)
     end
 
-    it "includes correct placeholder text in the response body" do
+    it 'includes correct placeholder text in the response body' do
       expect(response.body).to include('Here is a list of users')
     end
   end
 
-  describe "GET #show" do
-  let(:user) {User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer from USA.', posts_counter: 10)}
-  before { get user_path(user) }
+  describe 'GET #show' do
+    let(:user) { User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer from USA.', posts_counter: 10) }
+    before { get user_path(user) }
 
-  it "returns http success" do
-    expect(response).to have_http_status(:success)
-  end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
 
-  it "renders show template" do
-    expect(response).to render_template(:show)
-  end
+    it 'renders show template' do
+      expect(response).to render_template(:show)
+    end
 
-  it "includes correct placeholder text in the response body" do
-    expect(response.body).to include('Here is one user')
-  end
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('Here is one user')
+    end
   end
 end
