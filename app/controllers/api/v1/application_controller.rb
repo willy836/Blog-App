829a0e061b2ab6511da320bf2dee33d4fb0ba06e
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-15 12:45:37 +0500

    API key will be generate access token and authorized user find by id.

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,4 +2,11 @@ class Api::V1::ApplicationController < ActionController::API
     before_action :restrict_access
   respond_to :json
 
+  private
+ 
+  def restrict_access
+    api_key = ApiKey.find_by_access_token(params[:access_token])
+    head :unauthorized unless api_key
+    @user = User.find(params[:author_id])
+  end
 end
