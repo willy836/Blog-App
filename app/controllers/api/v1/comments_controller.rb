fba3269558d9263094903ac5e19e7cdf3ab07bf1
willy836 <wilsonkimiri836@gmail.com>
2023-04-15 10:17:29 +0300

    Refactor comments_controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,7 +1,33 @@
-class Api::V1::CommentsController < ApplicationController
+class Api::V1::CommentsController < Api::V1::ApplicationController
+  before_action :set_post, only: [:show, :create]
+  before_action :set_author, only: [:show, :create]
+
   def index
-    @post = Post.find(params[:post_id])
     @comments = @post.comments
-    render json: { data: @comments }, status: :ok
+    render json: { comments: @comments }, status: :ok
   end
+
+  
+  def create
+    @comment = @author.comments.new(post: @post, **comment_params)
+    if @comment.save
+      render json: { comment: @comment }, status: :created
+    else
+      render json: { error: @comment.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_author
+    @author = User.find(params[:author_id])
+  end
+
+  def set_post
+    @post = set_author.posts.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end 
 end
