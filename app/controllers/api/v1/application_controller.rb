a5aea606bb4385bb1506fc624c4d1912eb78242c
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-15 12:47:55 +0500

    fix rubocop

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::ApplicationController < ActionController::API
-    before_action :restrict_access
-    respond_to :json
+  before_action :restrict_access
+  respond_to :json
 
   private
- 
+
   def restrict_access
     api_key = ApiKey.find_by_access_token(params[:access_token])
     head :unauthorized unless api_key
